459d982db583eb540ba5d8d6959791ac05126cb1
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-11-30 08:40:57 +0100

    Deploy git-pages

---
+++ src/Fetch.js
@@ -0,0 +1,66 @@
+import './App.css';
+import axios from 'axios';
+import logo from './carambar_logo.png';
+
+import React, { useEffect, useState } from 'react';
+
+function Jokes() {
+  //show hide aswer
+  const [isVisible, setIsVisible] = useState(false);
+
+  //state for jokes
+  const [jokes, setJokes] = useState([]);
+
+  const FetchJoke = async () => {
+    try {
+      const response = await axios.get('https://carambar-jokes-api-7d22.onrender.com/blagues/random');
+      setJokes(response.data);
+      setIsVisible(false);
+    } catch (error) {
+      console.error('Error fetching joke', error);
+    }
+  };
+
+  //call fetchJoke one time []
+  useEffect(() => {
+    FetchJoke();
+  }, []);
+
+  return (
+    <>
+      <div className='app'>
+        <h1>Les blagues</h1>
+        <img src={logo} alt='Logo' />
+
+{/* show jokes  */}
+        {jokes ? (
+          <div className='card'>
+            <h2>{jokes.text}</h2>
+
+            <div
+              className='answer_joke'
+              style={{ display: isVisible ? 'block' : 'none' }}
+            >
+              <p>{jokes.answer}🤣</p>
+            </div>
+            <button
+              onClick={() => setIsVisible(!isVisible)}
+              className='btn answer'
+            >
+              La reponse
+            </button>
+          </div>
+        ) : (
+          // show loading while rearch joke
+          <p>Loading...</p>
+        )}
+
+        <button className='btn new_question' onClick={FetchJoke}>
+          Nouvelle blague
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Jokes;
